7179e7cf4b8fbf7df0a74d58c42a30a08cb02d71
DeveloperEmma20 <168389998+DeveloperEmma20@users.noreply.github.com>
2024-04-28 22:27:09 -0400

    Create classes.js

---
+++ Pages/script/classes.js
@@ -0,0 +1,14 @@
+var meals = [
+    {
+        "id": 0,
+        "description": "",
+        "picture": "",
+        "directions": "",
+        "ingredients": "",
+        "price": "",
+        "protein": ""
+    }
+];
+
+
+// const id = parseInt(meals[0].id);
